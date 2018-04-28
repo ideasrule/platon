2e89e5c7cb5c148c794864fd1180a2795553e1d6
Michael Zhang <zmzhang@caltech.edu>
2018-04-27 23:15:32 -0700

    Now using binary absorption files (it was never opacity; previous name was misleading
    Former-commit-id: 0e24ddaad54a4037af4af1ef133f0ad0e0ffd2fa

--- species_data_reader.py
+++ species_data_reader.py
@@ -2,59 +2,9 @@ import numpy as np
 import os
 from constants import k_B
 
-class OpacityData:
-    species_name = None
-    species_mass = None
-    filename = None
-    wavelengths = None
-    pressures = None
-    cross_sections = []
-    opacities = []
-    
-    def __init__(self, species_name, filename):
-        self.species_name = species_name
-        self.filename = filename
-        self.wavelengths, self.temperatures, self.pressures, self.cross_sections = self.read_opacity_file(filename)
-        self.opacities = self.get_opacity()
 
-    def read_opacity_file(self, filename):
-        line_counter = 0
-        temperatures = None
-
-        pressures = None        
-        wavelengths = []
-        cross_sections = []
-
-        with open(filename) as f:
-            curr_wavelength = None
-
-            for line in f:
-                elements = [float(e) for e in line.split()]
-                if line_counter == 0: temperatures = np.array(elements)
-                elif line_counter == 1: pressures = np.array(elements)
-                else:
-                    if len(elements) == 1:
-                        curr_wavelength = elements[0]
-                        wavelengths.append(curr_wavelength)
-                        cross_sections.append([])
-                    else:
-                        log_P = elements[0]
-                        cross_sections[-1].append(np.array(elements[1:]))
-                line_counter += 1
-
-        return np.array(wavelengths), np.array(temperatures), np.array(pressures), np.array(cross_sections)
-
-    def get_opacity(self):
-        opacities = np.zeros(self.cross_sections.shape)
-        for T_index in range(len(self.temperatures)):
-            for P_index in range(len(self.pressures)):
-                rho = k_B * self.temperatures[T_index]/self.pressures[P_index]
-                opacities[:, P_index, T_index] = self.cross_sections[:, P_index, T_index]/rho                
-        return opacities
-
-
-def read_species_data(opacity_dir, species_masses_file):
-    opacities_data = dict()
+def read_species_data(absorption_dir, species_masses_file, absorption_file_prefix="absorb_coeffs_"):
+    absorption_data = dict()
     mass_data = dict()
     
     with open(species_masses_file) as f:
@@ -64,11 +14,11 @@ def read_species_data(opacity_dir, species_masses_file):
             species_name = columns[0]
             print "Loading", species_name
             species_mass = float(columns[1])
-            opacity_filename = os.path.join(opacity_dir, "opac{0}.dat".format(species_name))
-            if os.path.isfile(opacity_filename):
-                opacities_data[species_name] = OpacityData(species_name, opacity_filename)
+            absorption_filename = os.path.join(absorption_dir, absorption_file_prefix + species_name + ".npy")
+            if os.path.isfile(absorption_filename):
+                absorption_data[species_name] = np.load(absorption_filename)
             mass_data[species_name] = species_mass
 
-    return opacities_data, mass_data
+    return absorption_data, mass_data
     
 
--- transit_depth_calculator.py
+++ transit_depth_calculator.py
@@ -10,16 +10,15 @@ import pickle
 from constants import k_B, amu
 
 class TransitDepthCalculator:
-    def __init__(self, planet_radius, star_radius, g, opacity_dir, species_masses_file):
+    def __init__(self, planet_radius, star_radius, g, absorption_dir="Absorption", species_masses_file="all_species_masses", lambda_grid_file="wavelengths.npy", P_grid_file="pressures.npy", T_grid_file="temperatures.npy"):
         self.planet_radius = planet_radius
         self.star_radius = star_radius
         self.g = g
-        self.opacities_data, self.mass_data = read_species_data(opacity_dir, species_masses_file)
+        self.absorption_data, self.mass_data = read_species_data(absorption_dir, species_masses_file)
 
-        sample_data = self.opacities_data.values()[0]
-        self.lambda_grid = sample_data.wavelengths
-        self.P_grid = sample_data.pressures
-        self.T_grid = sample_data.temperatures
+        self.lambda_grid = np.load(lambda_grid_file)
+        self.P_grid = np.load(P_grid_file)
+        self.T_grid = np.load(T_grid_file)
 
         self.N_lambda = len(self.lambda_grid)
         self.N_temperatures = len(self.T_grid)
@@ -28,24 +27,24 @@ class TransitDepthCalculator:
     def compute_depths(self, P, T, abundances):
         '''P: array of length N_tau
            T: array of length N_tau
-           abundances: dictionary mapping species name to (N_T, N_P) array, where N_T is the number of temperature points in the opacity data files, and N_P is the number of pressure points in those files'''
+           abundances: dictionary mapping species name to (N_T, N_P) array, where N_T is the number of temperature points in the absorption data files, and N_P is the number of pressure points in those files'''
         start = time.time()
         assert(len(P) == len(T))
         
         N_tau = len(P)
-        kappa = np.zeros((self.N_lambda, self.N_pressures, self.N_temperatures))
+        absorption_coeff = np.zeros((self.N_lambda, self.N_pressures, self.N_temperatures))
         mu = np.zeros(N_tau)
         
         for species_name in abundances.keys():
             assert(abundances[species_name].shape == (self.N_pressures, self.N_temperatures))
-            if species_name in self.opacities_data: 
-                kappa += self.opacities_data[species_name].opacities * abundances[species_name]
+            if species_name in self.absorption_data: 
+                absorption_coeff += self.absorption_data[species_name] * abundances[species_name]
             if species_name in self.mass_data:
                 interpolator = RectBivariateSpline(self.P_grid, self.T_grid, abundances[species_name], kx=1, ky=1)
                 atm_abundances = interpolator.ev(P, T)
                 mu += atm_abundances * self.mass_data[species_name]
             
-        kappa_atm = kappa_interpolator.fast_interpolate(kappa, self.T_grid, self.P_grid, T, P)
+        absorption_coeff_atm = kappa_interpolator.fast_interpolate(absorption_coeff, self.T_grid, self.P_grid, T, P)
         
         dP = P[1:] - P[0:-1]
         dh = dP/P[1:] * k_B * T[1:]/(mu[1:] * amu* self.g)
@@ -54,7 +53,7 @@ class TransitDepthCalculator:
         #dz goes from top to bottom of atmosphere
         radius_with_atm = np.sum(dh) + self.planet_radius
         heights = np.append(radius_with_atm, radius_with_atm - np.cumsum(dh))
-        tau_los = get_line_of_sight_tau(kappa_atm, heights)
+        tau_los = get_line_of_sight_tau(absorption_coeff_atm, heights)
 
         absorption_fraction = 1 - np.exp(-tau_los)
         transit_depths = (self.planet_radius/self.star_radius)**2 + 2/self.star_radius**2 * absorption_fraction.dot(heights[1:] * dh)
@@ -64,7 +63,7 @@ class TransitDepthCalculator:
         return transit_depths
         
         
-depth_calculator = TransitDepthCalculator(6.4e6, 7e8, 9.8, "Opac", "all_species_masses")
+depth_calculator = TransitDepthCalculator(6.4e6, 7e8, 9.8)
 
 index, P, T = np.loadtxt("T_P/t_p_800K.dat", unpack=True, skiprows=1)
 abundances = get_abundances("EOS/eos_1Xsolar_cond.dat")

6b1e55d08a91609cab667fa5cc22f793cc4e6611
Michael Zhang <zmzhang@caltech.edu>
2018-04-29 15:12:03 -0700

    Added scattering, collisional absorption
    Former-commit-id: fc4203d4255bc8a966e733da990924ef4bd46bca

--- species_data_reader.py
+++ species_data_reader.py
@@ -6,6 +6,7 @@ from constants import k_B
 def read_species_data(absorption_dir, species_masses_file, absorption_file_prefix="absorb_coeffs_"):
     absorption_data = dict()
     mass_data = dict()
+    polarizability_data = dict()
     
     with open(species_masses_file) as f:
         for line in f:
@@ -14,11 +15,15 @@ def read_species_data(absorption_dir, species_masses_file, absorption_file_prefi
             species_name = columns[0]
             print "Loading", species_name
             species_mass = float(columns[1])
+            polarizability = float(columns[2])
             absorption_filename = os.path.join(absorption_dir, absorption_file_prefix + species_name + ".npy")
             if os.path.isfile(absorption_filename):
                 absorption_data[species_name] = np.load(absorption_filename)
             mass_data[species_name] = species_mass
 
-    return absorption_data, mass_data
+            if polarizability != 0:
+                polarizability_data[species_name] = polarizability
+
+    return absorption_data, mass_data, polarizability_data
     
 
--- transit_depth_calculator.py
+++ transit_depth_calculator.py
@@ -10,21 +10,66 @@ import pickle
 from constants import k_B, amu
 
 class TransitDepthCalculator:
-    def __init__(self, planet_radius, star_radius, g, absorption_dir="Absorption", species_masses_file="all_species_masses", lambda_grid_file="wavelengths.npy", P_grid_file="pressures.npy", T_grid_file="temperatures.npy"):
+    def __init__(self, planet_radius, star_radius, g, absorption_dir="Absorption", species_masses_file="all_species_masses", lambda_grid_file="wavelengths.npy", P_grid_file="pressures.npy", T_grid_file="temperatures.npy", collisional_absorption_file="collisional_absorption.pkl"):
         self.planet_radius = planet_radius
         self.star_radius = star_radius
         self.g = g
-        self.absorption_data, self.mass_data = read_species_data(absorption_dir, species_masses_file)
+        self.absorption_data, self.mass_data, self.polarizability_data = read_species_data(absorption_dir, species_masses_file)
+
+        self.collisional_absorption_data = np.load(collisional_absorption_file)
 
         self.lambda_grid = np.load(lambda_grid_file)
         self.P_grid = np.load(P_grid_file)
         self.T_grid = np.load(T_grid_file)
 
         self.N_lambda = len(self.lambda_grid)
-        self.N_temperatures = len(self.T_grid)
-        self.N_pressures = len(self.P_grid)
+        self.N_T = len(self.T_grid)
+        self.N_P = len(self.P_grid)
 
-    def compute_depths(self, P, T, abundances):
+        self.scattering_absorption = np.zeros((self.N_lambda, self.N_P, self.N_T))
+        P_meshgrid, lambda_meshgrid, T_meshgrid = np.meshgrid(self.P_grid, self.lambda_grid, self.T_grid)
+        self.P_meshgrid = P_meshgrid
+        self.T_meshgrid = T_meshgrid
+        self.scattering_factor = 8*np.pi/3 * (2*np.pi/lambda_meshgrid)**4 * P_meshgrid / (k_B * T_meshgrid)
+
+
+    def get_gas_absorption(self, abundances):
+        absorption_coeff = np.zeros((self.N_lambda, self.N_P, self.N_T))
+         
+        for species_name in abundances.keys():
+            assert(abundances[species_name].shape == (self.N_P, self.N_T))
+            if species_name in self.absorption_data: 
+                absorption_coeff += self.absorption_data[species_name] * abundances[species_name]
+                
+        return absorption_coeff
+
+        
+    def get_scattering_absorption(self, abundances):
+        cross_section = np.zeros((self.N_lambda, self.N_P, self.N_T))
+        scatt_prefactor = 8*np.pi/3 * (2*np.pi/self.lambda_grid)**4
+        scatt_prefactor = scatt_prefactor.reshape((self.N_lambda,1,1))
+        
+        for species_name in abundances:
+            if species_name in self.polarizability_data:
+                cross_section += abundances[species_name] * self.polarizability_data[species_name]**2 * scatt_prefactor
+
+        return cross_section * self.P_meshgrid/(k_B*self.T_meshgrid)
+        
+    def get_collisional_absorption(self, abundances):
+        absorption_coeff = np.zeros((self.N_lambda, self.N_P, self.N_T))
+        
+        for s1, s2 in self.collisional_absorption_data:
+            if s1 in abundances and s2 in abundances:
+                n = self.P_meshgrid/(k_B*self.T_meshgrid)
+                n1 = abundances[s1]*n
+                n2 = abundances[s2]*n
+                abs_data = self.collisional_absorption_data[(s1,s2)].reshape((self.N_lambda, 1, self.N_T))
+                absorption_coeff += abs_data * n1 * n2
+
+        return absorption_coeff
+        
+
+    def compute_depths(self, P, T, abundances, add_scattering=True, add_collisional_absorption=True):
         '''P: array of length N_tau
            T: array of length N_tau
            abundances: dictionary mapping species name to (N_T, N_P) array, where N_T is the number of temperature points in the absorption data files, and N_P is the number of pressure points in those files'''
@@ -32,19 +77,23 @@ class TransitDepthCalculator:
         assert(len(P) == len(T))
         
         N_tau = len(P)
-        absorption_coeff = np.zeros((self.N_lambda, self.N_pressures, self.N_temperatures))
-        mu = np.zeros(N_tau)
+
+        absorption_coeff = self.get_gas_absorption(abundances)
+        if add_scattering: absorption_coeff += self.get_scattering_absorption(abundances)
+        if add_collisional_absorption: absorption_coeff += self.get_collisional_absorption(abundances)
         
-        for species_name in abundances.keys():
-            assert(abundances[species_name].shape == (self.N_pressures, self.N_temperatures))
-            if species_name in self.absorption_data: 
-                absorption_coeff += self.absorption_data[species_name] * abundances[species_name]
-            if species_name in self.mass_data:
-                interpolator = RectBivariateSpline(self.P_grid, self.T_grid, abundances[species_name], kx=1, ky=1)
-                atm_abundances = interpolator.ev(P, T)
-                mu += atm_abundances * self.mass_data[species_name]
-            
+        mu = np.zeros(N_tau)
+        for species_name in abundances:
+            interpolator = RectBivariateSpline(self.P_grid, self.T_grid, abundances[species_name], kx=1, ky=1)
+            atm_abundances = interpolator.ev(P, T)
+            mu += atm_abundances * self.mass_data[species_name]
+
         absorption_coeff_atm = interpolator_3D.fast_interpolate(absorption_coeff, self.T_grid, self.P_grid, T, P)
+
+        '''_, lambda_meshgrid = np.meshgrid(P, self.lambda_grid)
+        prefactor = 8*np.pi/3 * (2*np.pi/lambda_meshgrid)**4 * P / (k_B * T)
+        for species_name in self.polarizability_data.keys():
+            absorption_coeff_atm += self.polarizability_data[species_name]**2 * prefactor * test[species_name]'''
         
         dP = P[1:] - P[0:-1]
         dh = dP/P[1:] * k_B * T[1:]/(mu[1:] * amu* self.g)

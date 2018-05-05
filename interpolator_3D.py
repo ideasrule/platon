51dce386a4da250de4a6decb74d3fff3dab09eb5
Michael Zhang <zmzhang@caltech.edu>
2018-05-05 10:01:33 -0700

    Corrected wavelength binning; restricting calculations in lambda
    Former-commit-id: 9e333e24f5569e36276ff37771e46f07c4cbf8da

--- interpolator_3D.py
+++ interpolator_3D.py
@@ -1,28 +1,56 @@
 import numpy as np
 import sys
-from scipy.interpolate import RectBivariateSpline
+from scipy.interpolate import RectBivariateSpline, interp1d
 import time
+
+def get_condition_array(target_data, interp_data):
+    cond = np.zeros(len(interp_data), dtype=bool)
+
+    start_index = None
+    end_index = None
+    for i in range(len(cond)):
+        if start_index is None:
+            if interp_data[i] > np.min(target_data): start_index = i-1
+            if interp_data[i] == np.min(target_data): start_index = i
+        if end_index is None:
+            if interp_data[i] >= np.max(target_data): end_index = i+1
+                
+    cond[start_index : end_index] = True
+    return cond
 
 def normal_interpolate(data, grid_x, grid_y, target_x, target_y):
     all_results = []
     for i in range(data.shape[0]):
-        interpolator = RectBivariateSpline(grid_x, grid_y, data[i], kx=1, ky=1)
-        result = interpolator.ev(target_x, target_y)
+        interpolator = RectBivariateSpline(grid_y, grid_x, data[i], kx=1, ky=1)
+        result = interpolator.ev(target_y, target_x)
         all_results.append(result)
     return np.array(all_results)
     
 
 def fast_interpolate(data, grid_x, grid_y, target_x, target_y):
-    T_mesh, P_mesh = np.meshgrid(np.arange(len(grid_x)), np.arange(len(grid_y)))
-    interpolator = RectBivariateSpline(grid_x, grid_y, T_mesh.T, kx=1, ky=1)
+    assert(len(target_x) == len(target_y))
+    
+    x_mesh, y_mesh = np.meshgrid(np.arange(len(grid_x)), np.arange(len(grid_y)))
+    if len(grid_x) == 1:
+        #Stupid hack to get around refusal of RectBivariateSpline to
+        #interpolate with only one element
+        interpolator = interp1d(grid_y, y_mesh.T.flatten())
+        y_indices = interpolator(target_y)
+        y_indices_lower = y_indices.astype(int)
+        y_indices_upper = y_indices_lower + 1
+        y_indices_frac = y_indices - y_indices_lower
+        return data[:, y_indices_lower, 0]*(1-y_indices_frac) + \
+            data[:, y_indices_upper, 0] * y_indices_frac
+                                           
+    interpolator = RectBivariateSpline(grid_x, grid_y, x_mesh.T, kx=1, ky=1)
     x_indices = interpolator.ev(target_x, target_y)
-
+    interpolator = RectBivariateSpline(grid_x, grid_y, y_mesh.T, kx=1, ky=1)
+    y_indices = interpolator.ev(target_x, target_y)
+    
     x_indices_lower = x_indices.astype(int)
     x_indices_upper = x_indices_lower + 1
     x_indices_frac = x_indices - x_indices_lower
-
-    interpolator = RectBivariateSpline(grid_x, grid_y, P_mesh.T, kx=1, ky=1)
-    y_indices = interpolator.ev(target_x, target_y)
+        
     y_indices_lower = y_indices.astype(int)
     y_indices_upper = y_indices_lower + 1
     y_indices_frac = y_indices - y_indices_lower
--- transit_depth_calculator.py
+++ transit_depth_calculator.py
@@ -31,85 +31,69 @@ class TransitDepthCalculator:
         self.T_meshgrid = T_meshgrid
 
         self.wavelength_rebinned = False
-            
-    def __rebin_absorption_dict__(self, bins, dictionary):
-        new_dict = dict()
+        self.wavelength_bins = None
         
-        for key in dictionary:
-            shape = dictionary[key].shape
-            assert(dictionary[key].shape[0] == self.N_lambda)
-
-            new_shape = list(shape)
-            new_shape[0] = len(bins)
-            
-            new_dict[key] = np.zeros(new_shape)           
-            for i, (start, end) in enumerate(bins):
-                selection = np.logical_and(self.lambda_grid >= start, self.lambda_grid < end)
-                averaged = np.mean(dictionary[key][selection], axis=0)
-                new_dict[key][i] = averaged
-
-        return new_dict
-        
+   
     def change_wavelength_bins(self, bins):
         if self.wavelength_rebinned:
             raise NotImplementedError("Multiple re-binnings not yet supported")
 
         self.wavelength_rebinned = True
+        self.wavelength_bins = bins
+
+        cond = np.any([np.logical_and(self.lambda_grid > start, self.lambda_grid < end) for (start,end) in bins], axis=0)
+
+        for key in self.absorption_data:
+            self.absorption_data[key] = self.absorption_data[key][cond]
+
+        for key in self.collisional_absorption_data:
+            self.collisional_absorption_data[key] = self.collisional_absorption_data[key][cond]
         
-        bins = np.array(bins)
-        if bins.ndim == 1:
-            new_bins = []
-            for i in range(len(bins)-1):
-                new_bins.append([bins[i], bins[i+1]])
-        bins = np.array(new_bins)
-        
-        self.absorption_data = self.__rebin_absorption_dict__(bins, self.absorption_data)
-        self.collisional_absorption_data = self.__rebin_absorption_dict__(bins, self.collisional_absorption_data)
-        self.lambda_grid = np.array([np.mean([start, end]) for (start, end) in bins])
-        self.N_lambda = len(bins)
+        self.lambda_grid = self.lambda_grid[cond]
+        self.N_lambda = len(self.lambda_grid)
         
         P_meshgrid, lambda_meshgrid, T_meshgrid = np.meshgrid(self.P_grid, self.lambda_grid, self.T_grid)
         self.P_meshgrid = P_meshgrid
         self.T_meshgrid = T_meshgrid
         
         
-    def get_gas_absorption(self, abundances):
-        absorption_coeff = np.zeros((self.N_lambda, self.N_P, self.N_T))
+    def get_gas_absorption(self, abundances, T_cond):
+        absorption_coeff = np.zeros((self.N_lambda, self.N_P, np.sum(T_cond)))
          
         for species_name in abundances.keys():
             assert(abundances[species_name].shape == (self.N_P, self.N_T))
-            if species_name in self.absorption_data: 
-                absorption_coeff += self.absorption_data[species_name] * abundances[species_name]
+            if species_name in self.absorption_data:
+                absorption_coeff += self.absorption_data[species_name][:,:,T_cond] * abundances[species_name][:,T_cond]
                 
         return absorption_coeff
 
         
-    def get_scattering_absorption(self, abundances):
-        cross_section = np.zeros((self.N_lambda, self.N_P, self.N_T))
+    def get_scattering_absorption(self, abundances, T_cond):
+        cross_section = np.zeros((self.N_lambda, self.N_P, np.sum(T_cond)))
         scatt_prefactor = 8*np.pi/3 * (2*np.pi/self.lambda_grid)**4
         scatt_prefactor = scatt_prefactor.reshape((self.N_lambda,1,1))
         
         for species_name in abundances:
             if species_name in self.polarizability_data:
-                cross_section += abundances[species_name] * self.polarizability_data[species_name]**2 * scatt_prefactor
-
-        return cross_section * self.P_meshgrid/(k_B*self.T_meshgrid)
+                cross_section += abundances[species_name][:,T_cond] * self.polarizability_data[species_name]**2 * scatt_prefactor
+        
+        return cross_section * self.P_meshgrid[:,:,T_cond]/(k_B*self.T_meshgrid[:,:,T_cond])
 
     
-    def get_collisional_absorption(self, abundances):
-        absorption_coeff = np.zeros((self.N_lambda, self.N_P, self.N_T))
-        n = self.P_meshgrid/(k_B * self.T_meshgrid)                    
+    def get_collisional_absorption(self, abundances, T_cond):
+        absorption_coeff = np.zeros((self.N_lambda, self.N_P, np.sum(T_cond)))
+        n = self.P_meshgrid[:,:,T_cond]/(k_B * self.T_meshgrid[:,:,T_cond])
         
         for s1, s2 in self.collisional_absorption_data:
             if s1 in abundances and s2 in abundances:
-                n1 = abundances[s1]*n
-                n2 = abundances[s2]*n
-                abs_data = self.collisional_absorption_data[(s1,s2)].reshape((self.N_lambda, 1, self.N_T))
+                n1 = (abundances[s1][:,T_cond]*n)
+                n2 = (abundances[s2][:,T_cond]*n)
+                abs_data = self.collisional_absorption_data[(s1,s2)].reshape((self.N_lambda, 1, self.N_T))[:,:,T_cond]
                 absorption_coeff += abs_data * n1 * n2
 
         return absorption_coeff
         
-
+    
     def compute_depths(self, P, T, abundances, add_scattering=True, add_collisional_absorption=True, cloudtop_pressure=np.inf):
         '''
         P: List of pressures in atmospheric P-T profile, in ascending order
@@ -121,9 +105,11 @@ class TransitDepthCalculator:
         start = time.time()
         assert(len(P) == len(T))
 
-        absorption_coeff = self.get_gas_absorption(abundances)
-        if add_scattering: absorption_coeff += self.get_scattering_absorption(abundances)
-        if add_collisional_absorption: absorption_coeff += self.get_collisional_absorption(abundances)
+        T_cond = interpolator_3D.get_condition_array(T, self.T_grid)
+
+        absorption_coeff = self.get_gas_absorption(abundances, T_cond)
+        if add_scattering: absorption_coeff += self.get_scattering_absorption(abundances, T_cond)
+        if add_collisional_absorption: absorption_coeff += self.get_collisional_absorption(abundances, T_cond)
         
         mu = np.zeros(len(P))
         for species_name in abundances:
@@ -131,7 +117,7 @@ class TransitDepthCalculator:
             atm_abundances = interpolator.ev(P, T)
             mu += atm_abundances * self.mass_data[species_name]
 
-        absorption_coeff_atm = interpolator_3D.fast_interpolate(absorption_coeff, self.T_grid, self.P_grid, T, P)
+        absorption_coeff_atm = interpolator_3D.fast_interpolate(absorption_coeff, self.T_grid[T_cond], self.P_grid, T, P)
 
         dP = P[1:] - P[0:-1]
         dr = dP/P[1:] * k_B * T[1:]/(mu[1:] * amu* self.g)
@@ -147,39 +133,41 @@ class TransitDepthCalculator:
         absorption_fraction[:, P > cloudtop_pressure] = 0
         
         transit_depths = (self.planet_radius/self.star_radius)**2 + 2/self.star_radius**2 * absorption_fraction.dot(radii[1:] * dr)
-
         end = time.time()
         print "Time taken", end-start
-        return transit_depths
+        
+        binned_wavelengths = []
+        binned_depths = []
+        if self.wavelength_bins is not None:
+            for (start, end) in self.wavelength_bins:
+                cond = np.logical_and(self.lambda_grid >= start, self.lambda_grid < end)
+                binned_wavelengths.append(np.mean(self.lambda_grid[cond]))
+                binned_depths.append(np.mean(transit_depths[cond]))
+            return np.array(binned_wavelengths), np.array(binned_depths)
+        
+        return self.lambda_grid, transit_depths
         
 
 index, P, T = np.loadtxt("T_P/t_p_800K.dat", unpack=True, skiprows=1)
+T = T*0.9
 abundances = eos_reader.get_abundances("EOS/eos_1Xsolar_cond.dat")
     
 depth_calculator = TransitDepthCalculator(6.4e6, 7e8, 9.8)
-#depth_calculator.change_wavelength_bins([[1e-6,2e-6], [2e-6,3e-6]])
-#depth_calculator.change_wavelength_bins(np.linspace(1.1e-6, 1.7e-6, 30))
+wfc_wavelengths = np.linspace(1.1e-6, 1.7e-6, 30)
+wavelength_bins = []
+for i in range(len(wfc_wavelengths) - 1):
+    wavelength_bins.append([wfc_wavelengths[i], wfc_wavelengths[i+1]])
 
-transit_depths = depth_calculator.compute_depths(P, T, abundances, cloudtop_pressure=np.inf)
+wavelength_bins.append([3.2e-6, 4e-6])
+wavelength_bins.append([4e-6, 5e-6])
+depth_calculator.change_wavelength_bins(wavelength_bins)
+
+wavelengths, transit_depths = depth_calculator.compute_depths(P, T, abundances, cloudtop_pressure=np.inf)
 transit_depths *= 100
-
-#plt.plot(depth_calculator.lambda_grid, transit_depths)
-#plt.show()
 
 ref_wavelengths, ref_depths = np.loadtxt("ref_spectra.dat", unpack=True, skiprows=2)
 plt.plot(ref_wavelengths, ref_depths, label="ExoTransmit")
-plt.plot(depth_calculator.lambda_grid, transit_depths, label="PyExoTransmit")
+plt.plot(wavelengths, transit_depths, label="PyExoTransmit")
 plt.legend()
-plt.figure()
-plt.title("Residuals of comparison")
-plt.plot(ref_wavelengths, ref_depths-transit_depths)
-
-plt.figure()
-
-depth_calculator.change_wavelength_bins(np.linspace(1.1e-6, 1.7e-6, 30))
-binned_transit_depths = depth_calculator.compute_depths(P, T, abundances) * 100
-plt.plot(ref_wavelengths, transit_depths, label="Unbinned")
-plt.plot(depth_calculator.lambda_grid, binned_transit_depths, label="Binned")
-
 plt.show()
 

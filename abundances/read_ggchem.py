93b871b7219682a7d418d33c8108775cd206c2b6
Michael Zhang <zmzhang@caltech.edu>
2018-05-15 02:09:03 -0700

    Code for reading ggchem output
    Former-commit-id: f1d805e266cac3002f2cddc42cf27be80c42f331

---
+++ abundances/read_ggchem.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pickle
+import sys
+
+def get_species_from_file(filename):
+    species = []
+    for line in open(filename):
+        species.append(line.split()[0])
+    return species
+
+
+filename = sys.argv[1]
+species_info_file = sys.argv[2]
+
+N_T = 30
+N_P = 13
+
+header = None
+num_atoms = None
+num_molecules = None
+
+abund_dict = dict()
+
+
+for i, line in enumerate(open(filename)):
+    elements = line.split()
+    if i == 1:
+        num_atoms = int(elements[0])
+        num_molecules = int(elements[1])
+    if i == 2: header = elements
+    if i > 2: break
+
+data = np.loadtxt(filename, skiprows=3)
+species_to_include = get_species_from_file(species_info_file)
+include = [h in species_to_include for h in header]
+
+for i,row in enumerate(data):
+    total = np.sum(10**row[include])
+    print total
+    data[i, include] = 10**row[include]/total
+
+#print header
+for i, h in enumerate(header):
+    abund_dict[h] = data[:,i].reshape((N_P, N_T))
+    abund_dict[h] = np.flip(abund_dict[h], 0)
+    abund_dict[h] = np.flip(abund_dict[h], 1)
+
+
+pickle.dump(abund_dict, open("output.pkl", "wb"))    
+#print abund_dict.keys()
+

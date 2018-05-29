9181ba66ef857e6f65b10aad9d8282b02e6cd887
Michael Zhang <zmzhang@caltech.edu>
2018-05-29 01:20:16 -0700

    Fixing transit depth unit test; now it uses hot Jupiter
    Former-commit-id: 324cb0d7c875310031244973aa3cd4d58410dab7

--- test_transit_depth_calculator.py
+++ test_transit_depth_calculator.py
@@ -7,40 +7,40 @@ import eos_reader
 from transit_depth_calculator import TransitDepthCalculator
 
 class TestTransitDepthCalculator(unittest.TestCase):
-    def test_EOS_file(self):    
+    def get_frac_dev(self, logZ, CO_ratio, custom_abundances):
         depth_calculator = TransitDepthCalculator(7e8, 9.8, max_P_profile=1.014e5)
-        custom_abundances = eos_reader.get_abundances("EOS/eos_1Xsolar_cond.dat")
-        wavelengths, transit_depths = depth_calculator.compute_depths(6.4e6, 800, custom_abundances=custom_abundances)
+        wavelengths, transit_depths = depth_calculator.compute_depths(7.14e7, 1200, logZ=logZ, CO_ratio=CO_ratio, custom_abundances=custom_abundances)
 
-        ref_wavelengths, ref_depths = np.loadtxt("testing_data/ref_spectra.dat", unpack=True, skiprows=2)
+        # This ExoTransmit run is done without SH, since it's not present in
+        # GGchem
+        ref_wavelengths, ref_depths = np.loadtxt("testing_data/hot_jupiter_spectra.dat", unpack=True, skiprows=2)
         ref_depths /= 100
-
+                
         frac_dev = np.abs(ref_depths - transit_depths) / ref_depths
-        self.assertTrue(np.all(frac_dev < 0.01))
+
+        '''plt.plot(wavelengths, transit_depths, label="PyExoTransmit")
+        plt.plot(ref_wavelengths, ref_depths, label="ExoTransmit")
+        plt.legend()
+
+        plt.figure()
+        plt.plot(np.log10(frac_dev))
+        plt.show()'''
+        
+        return frac_dev
+        
+    def test_EOS_file(self):
+        custom_abundances = eos_reader.get_abundances("EOS/eos_1Xsolar_cond.dat")
+        custom_abundances["SH"] *= 0
+        frac_dev = self.get_frac_dev(None, None, custom_abundances)
+        self.assertTrue(np.all(frac_dev < 0.02))
 
     def test_ggchem(self):
-        depth_calculator = TransitDepthCalculator(7e8, 9.8, include_condensates=True, max_P_profile=1.014e5)
-        wavelengths, transit_depths = depth_calculator.compute_depths(6.4e6, 800, 0, 0.53)
-
-        ref_wavelengths, ref_depths = np.loadtxt("testing_data/ref_spectra.dat", unpack=True, skiprows=2)
-        ref_depths /= 100
-
-        frac_dev = np.abs(ref_depths - transit_depths) / ref_depths
+        frac_dev = self.get_frac_dev(0, 0.53, None)
 
         #Some, but very few, individual lines are highly discrepant
-        self.assertLess(np.percentile(frac_dev, 95), 0.01)
+        self.assertLess(np.percentile(frac_dev, 95), 0.02)
 
-        #These lines are sodium and potassium lines; abundances are discrepant
-        #between GGchem and Eliza's code. We mask out those lines
-
-        frac_dev[np.logical_and(wavelengths > 403e-9, wavelengths < 406e-9)] = 0
-        frac_dev[np.logical_and(wavelengths > 580e-9, wavelengths < 600e-9)] = 0
-        frac_dev[np.logical_and(wavelengths > 750e-9, wavelengths < 780e-9)] = 0
-
-        #This is a HCl line.  In this case, ExoTransmit does not predict
-        #HCl abundances correctly
-        frac_dev[np.logical_and(wavelengths > 3.76e-6, wavelengths < 3.78e-6)] = 0
-        self.assertLess(np.max(frac_dev), 0.02)
+        self.assertLess(np.max(frac_dev), 0.03)
         
 
 

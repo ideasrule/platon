0a9c5111b197b0b15a6eecc4cf4e9a9781833eb0
Michael Zhang <mzzhang2014@gmail.com>
2018-06-05 15:22:20 -0700

    Updated version in docs
    Former-commit-id: b49185443288589a89d5ae3e92442b4a5e311885

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Michael Zhang, Yayaati Chachan, Eliza Kempton'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.0alpha'
+release = u'1.1beta'
 
 
 # -- General configuration ---------------------------------------------------

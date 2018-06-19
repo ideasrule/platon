4edda13ec394fa4cf8df43ef764dabd9801edacd
Michael Zhang <zmzhang@caltech.edu>
2018-06-19 00:56:06 -0700

    Fixed download URL
    Former-commit-id: 2faa2be7e4fa79ebb459517bb591278ec074407e

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,3 +1,3 @@
 name = "platon"
 __version__ = "1.0"
-__data_url__ = "https://github.com/ideasrule/platon/releases/download/1.0/beta.zip"
+__data_url__ = "https://github.com/ideasrule/platon/releases/download/beta/data.zip"

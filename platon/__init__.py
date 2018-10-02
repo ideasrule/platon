3f6bdf90a2953bda6b546c525ddc219ff0dc9fe8
Michael Zhang <stanley@Michaels-MacBook-Pro.local>
2018-10-01 18:34:07 -0700

    Incrementing version
    Former-commit-id: f77b1cf0d09222d8802e5311c255836acd0c832a

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,3 +1,3 @@
 name = "platon"
-__version__ = "1.1"
-__data_url__ = "https://github.com/ideasrule/platon/releases/download/v1.1/data.zip"
+__version__ = "2.0"
+__data_url__ = "https://github.com/ideasrule/platon/releases/download/v2.0/data.zip"

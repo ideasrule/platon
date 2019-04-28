ade18dc12567798d471c4ead48da984e4907b7ff
Michael Zhang <zmzhang@caltech.edu>
2019-04-28 14:23:17 -0700

    Incrementing version number to 4
    Former-commit-id: 104fa5cbf2e0db451772b92b1958b7b64255cc0f

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,3 +1,3 @@
 name = "platon"
-__version__ = "3.1"
-__data_url__ = "https://github.com/ideasrule/platon/releases/download/v3.0/data.zip"
+__version__ = "4.0"
+__data_url__ = "https://github.com/ideasrule/platon/releases/download/v4.0/data.zip"

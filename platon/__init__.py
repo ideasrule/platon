933735bdf849b0ff7c64af200820b0e4fa628f72
Michael Zhang <mzzhang2014@gmail.com>
2020-04-21 19:16:20 -0700

    Updating version number for bug fix

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,4 +1,4 @@
 name = "platon"
-__version__ = "5.1"
+__version__ = "5.1.1"
 __md5sum__ = "adb9ee511904717bf864786dcddcbacc"
 __data_url__ = "https://www.astro.caltech.edu/platon/data_{}.zip".format(__md5sum__)

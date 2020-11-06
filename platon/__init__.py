0bd3153487ad22e22952af7db345a44b85751fc6
Michael Zhang <mzzhang2014@gmail.com>
2020-11-06 12:18:24 -0800

    Incrementing version number after fixing data URL

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,4 +1,4 @@
 name = "platon"
-__version__ = "5.1.1"
+__version__ = "5.1.2"
 __md5sum__ = "adb9ee511904717bf864786dcddcbacc"
 __data_url__ = "https://sites.astro.caltech.edu/platon/data_{}.zip".format(__md5sum__)

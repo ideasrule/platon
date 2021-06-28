6ba7d7248170f6177430dbdf81625c6de997bb41
Michael Zhang <mzzhang2014@gmail.com>
2021-06-28 14:42:54 -0700

    Fixed bug that caused nans in K opacity.  Added in opacity from other lines for Na and K, not just the doublet.  The stitching is rudimentary.

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,4 +1,4 @@
 name = "platon"
-__version__ = "5.2"
-__md5sum__ = "54b5eec1b91051d3f74f5f467e399161"
+__version__ = "5.3"
+__md5sum__ = "87bf20c5c79254e6a2d1bfe16dea3f53"
 __data_url__ = "https://sites.astro.caltech.edu/platon/data_{}.zip".format(__md5sum__)

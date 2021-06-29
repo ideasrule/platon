c7b2f6d1e72e1ab006844e77b1dc10788da27916
Michael Zhang <mzzhang2014@gmail.com>
2021-06-29 09:50:29 -0700

    Cleaning up retrieval example

--- examples/retrieve_brown_dwarf.py
+++ examples/retrieve_brown_dwarf.py
@@ -9,9 +9,8 @@ from platon.constants import R_sun, R_jup, M_jup, AU, PC, h, c
 from scipy.ndimage import uniform_filter
 
 wavelengths, fluxes, errors = np.loadtxt(sys.argv[1], unpack=True, skiprows=1, delimiter=",")
-#plt.errorbar(wavelengths, fluxes, yerr=errors)
-#plt.show()
 
+#To SI
 wavelengths *= 1e-6
 fluxes *= 1e6
 errors *= 1e6
@@ -30,15 +29,6 @@ errors = uniform_filter(errors, factor)[::factor] * 2
 width = np.median(np.diff(wavelengths))
 bins = np.array([wavelengths - width/2, wavelengths + width/2]).T
 
-#Fluxes to photon fluxes
-#factor = width / (h * c / wavelengths)
-#fluxes *= factor
-#errors *= factor
-
-plt.errorbar(wavelengths, fluxes, yerr=errors, fmt='.')
-#plt.figure()
-#plt.plot(np.diff(wavelengths))
-#plt.show()
 
 #create a Retriever object
 retriever = Retriever()
@@ -55,31 +45,25 @@ fit_info = retriever.get_default_fit_info(
     profile_type="radiative_solution" #"isothermal" for isothermal fitting
     )
 
-#Add fitting parameters - this specifies which parameters you want to fit
-#e.g. since we have not included cloudtop_P, it will be fixed at the value specified in the constructor
+#Add fitting parameters 
 
 #Physical parameters
 fit_info.add_uniform_fit_param("Mp", 5*M_jup, 30*M_jup)
 fit_info.add_uniform_fit_param("Rp", 1*R_jup, 4*R_jup)
 
 #Chemistry
-#fit_info.add_uniform_fit_param("logZ", -1, 3)
-#fit_info.add_uniform_fit_param("CO_ratio", 0.2, 2)
+fit_info.add_uniform_fit_param("logZ", -1, 3)
+fit_info.add_uniform_fit_param("CO_ratio", 0.2, 2)
 
 #T/P profile parameters
-#fit_info.add_uniform_fit_param("T", 100, 3000)
 fit_info.add_uniform_fit_param("T_int", 100, 2500)
 fit_info.add_uniform_fit_param("log_k_th", -5, 0)
-#fit_info.add_uniform_fit_param("log_gamma", -4, 1)
-#fit_info.add_uniform_fit_param("log_gamma2", -4, 1)
-#fit_info.add_uniform_fit_param("alpha", 0, 0.5)
-
 
 #Use Nested Sampling to do the fitting
 result = retriever.run_multinest(bins, fluxes, errors,
                                  fit_info, plot_best=True, nlive=200,
                                  sample="rwalk",
-                                 rad_method="xsec") #"ktables" instead of "xsec" for correlated k
+                                 rad_method="xsec") 
 
 with open("example_retrieval_result.pkl", "wb") as f:
     pickle.dump(result, f)

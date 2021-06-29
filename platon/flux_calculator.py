87554b40ce5bb93f2e119c510eee8f7d440a6eed
Michael Zhang <mzzhang2014@gmail.com>
2021-06-28 17:59:46 -0700

    Updating documentation

--- platon/flux_calculator.py
+++ platon/flux_calculator.py
@@ -93,10 +93,8 @@ class FluxCalculator:
                        add_collisional_absorption=True,
                        cloudtop_pressure=np.inf, custom_abundances=None,
                        custom_atm_abundances=None,
-                       T_spot=None, spot_cov_frac=None,
                        ri = None, frac_scale_height=1,number_density=0,
                        part_size=1e-6, part_size_std=0.5, P_quench=1e-99,
-                       stellar_blackbody=False,
                        full_output=False):
         '''Most parameters are explained in :func:`~platon.transit_depth_calculator.TransitDepthCalculator.compute_depths`
 
@@ -104,11 +102,79 @@ class FluxCalculator:
         ----------
         t_p_profile : Profile
             A Profile object from TP_profile
+        planet_mass : float
+            Mass of the planet, in kg
+        planet_radius : float
+            Radius of the planet at 100,000 Pa. Must be in metres.
+        dist : float
+            Distance of the planet, in meters.
+        logZ : float
+            Base-10 logarithm of the metallicity, in solar units
+        CO_ratio : float, optional
+            C/O atomic ratio in the atmosphere.  The solar value is 0.53.
+        add_gas_absorption: float, optional
+            Whether gas absorption is accounted for
+        add_H_minus_absorption: float, optional
+            Whether H- bound-free and free-free absorption is added in
+        add_scattering : bool, optional
+            whether Rayleigh scattering is taken into account
+        scattering_factor : float, optional
+            if `add_scattering` is True, make scattering this many
+            times as strong. If `scattering_slope` is 4, corresponding to
+            Rayleigh scattering, the absorption coefficients are simply
+            multiplied by `scattering_factor`. If slope is not 4,
+            `scattering_factor` is defined such that the absorption coefficient
+            is that many times as strong as Rayleigh scattering at
+            `scattering_ref_wavelength`.
+        scattering_slope : float, optional
+            Wavelength dependence of scattering, with 4 being Rayleigh.
+        scattering_ref_wavelength : float, optional
+            Scattering is `scattering_factor` as strong as Rayleigh at this
+            wavelength, expressed in metres.
+        add_collisional_absorption : float, optional
+            Whether collisionally induced absorption is taken into account
+        cloudtop_pressure : float, optional
+            Pressure level (in Pa) below which light cannot penetrate.
+            Use np.inf for a cloudless atmosphere.
+        custom_abundances : str or dict of np.ndarray, optional
+            If specified, overrides `logZ` and `CO_ratio`.  Can specify a
+            filename, in which case the abundances are read from a file in the
+            format of the EOS/ files.  These are identical to ExoTransmit's
+            EOS files.  It is also possible, though highly discouraged, to
+            specify a dictionary mapping species names to numpy arrays, so that
+            custom_abundances['Na'][3,4] would mean the fractional number
+            abundance of Na at a temperature of self.T_grid[3] and pressure of
+            self.P_grid[4].
+        custom_atm_abundances : dict of np.ndarray, optional
+            If specified, overrides `logZ` and `CO_ratio`.  Each item in the 
+            dict specifies the abundance of one species at each of the layers 
+            in t_p_profile.  
+        ri : complex, optional
+            Complex refractive index n - ik (where k > 0) of the particles
+            responsible for Mie scattering.  If provided, Mie scattering will
+            be computed.  In that case, scattering_factor and scattering_slope
+            must be set to 1 and 4 (the default values) respectively.
+        frac_scale_height : float, optional
+            The number density of Mie scattering particles is proportional to
+            P^(1/frac_scale_height).  This is similar to, but a bit different
+            from, saying that the scale height of the particles is
+            frac_scale_height times that of the gas.
+        number_density: float, optional
+            The number density (in m^-3) of Mie scattering particles
+        part_size : float, optional
+            The mean radius of Mie scattering particles.  The distribution is
+            assumed to be log-normal, with a standard deviation of part_size_std
+        part_size_std : float, optional
+            The geometric standard deviation of particle radii. We recommend
+            leaving this at the default value of 0.5.
+        P_quench : float, optional
+            Quench pressure in Pa.
+        full_output : bool, optional
+            If True, returns info_dict as a third return value.
         '''
         T_profile = t_p_profile.temperatures
         P_profile = t_p_profile.pressures
-        #plt.semilogy(T_profile, P_profile)
-        #plt.show()
+
         atm_info = self.atm.compute_params(
             planet_mass, planet_radius, P_profile, T_profile,
             logZ, CO_ratio, add_gas_absorption, add_H_minus_absorption, add_scattering,

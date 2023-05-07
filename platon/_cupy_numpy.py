5533c72ed65f6a3096d722afa4c67a2db6626c51
Joseph Yeung <josephyeung@uchicago.edu>
2023-05-07 15:33:07 -0500

    added option to force CPU to _cupy_numpy.py

--- platon/_cupy_numpy.py
+++ platon/_cupy_numpy.py
@@ -1,12 +1,19 @@
+FORCE_CPU = True  # force to use CPU if this is True
+
 try:
-    from cupy import *
-    from cupyx import scipy
-except:
-    print("cupy not found. Disabling GPU acceleration")
+    if not FORCE_CPU:
+        from cupy import *
+        from cupyx import scipy
+        print("GPU acceleration enabled")
+    else:
+        raise ImportError
+except ImportError:
+    if not FORCE_CPU:
+        print("cupy not found. Disabling GPU acceleration")
     from numpy import *
     import scipy
     import scipy.special
-    
+
 def cpu(arr):
     try:
         return arr.get()

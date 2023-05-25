fa38e3168affe69c70a50c5dbc4ff9de3ca04cc5
Joseph Yeung <josephyeung@uchicago.edu>
2023-05-25 15:38:40 -0500

    fixed indentation error for _cupy_numpy.py

--- platon/_cupy_numpy.py
+++ platon/_cupy_numpy.py
@@ -8,13 +8,13 @@ if FORCE_CPU:
 
 else:
     try:
-    from cupy import *
-    from cupyx import scipy
-except:
-    print("cupy not found. Disabling GPU acceleration")
-    from numpy import *
-    import scipy
-    import scipy.special
+        from cupy import *
+        from cupyx import scipy
+    except:
+        print("cupy not found. Disabling GPU acceleration")
+        from numpy import *
+        import scipy
+        import scipy.special
 
 def cpu(arr):
     try:

13d2121a5509a8a7ec6c89a825aa57c702ad3b89
Michael Zhang <zmzhang@uchicago.edu>
2024-08-08 18:03:12 -0500

    Updating all data files to new resolution, new temperature grid, new wavelength grid, new line lists, new CIA files

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,4 +1,4 @@
 name = "platon"
 __version__ = "6.1"
-__md5sum__ = "79b7e7f41e015120830497508f2fcfeb"
+__md5sum__ = "f43fb69c4edd221bb304b9bd7064d727"
 __data_url__ = "https://astro.uchicago.edu/~mz/data_{}.zip".format(__md5sum__)

bae591b744512ba33da29e87a154a1c7667d517f
Michael Zhang <zmzhang@uchicago.edu>
2024-10-28 19:34:39 -0500

    Adding Kim's surface emission data. Modifying Na/K opacity data to fix bug (?) in previous version

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,4 +1,4 @@
 name = "platon"
 __version__ = "6.1"
-__md5sum__ = "f43fb69c4edd221bb304b9bd7064d727"
+__md5sum__ = "2cc3d6d746c80cc38bd9fdb25c226842"
 __data_url__ = "https://astro.uchicago.edu/~mz/data_{}.zip".format(__md5sum__)

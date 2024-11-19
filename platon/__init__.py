44120c7948964a4d8fee38eca0729552b5161227
Michael Zhang <zmzhang@uchicago.edu>
2024-11-18 18:45:10 -0600

    Fixed bug where last parameter was cut off for pymultinest

--- platon/__init__.py
+++ platon/__init__.py
@@ -1,4 +1,4 @@
 name = "platon"
-__version__ = "6.2.1"
+__version__ = "6.2.2"
 __md5sum__ = "2cc3d6d746c80cc38bd9fdb25c226842"
 __data_url__ = "https://astro.uchicago.edu/~mz/data_{}.zip".format(__md5sum__)
